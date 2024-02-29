712761339bb7755ad5d807680f8a4944d74a4ba6
YousefSaeed668 <yousefsaeed668@gmail.com>
2024-02-29 18:50:56 +0200

    Add new files and dependencies, complete the landing page and navbar initially

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,6 @@
-import clsx, { ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  // twMerge is a function from tailwind-merge that merges tailwind classes for example "px-2 py-2" it will make it "py-2"
-  return twMerge(clsx(inputs));
-  // clsx is a function that make the order of the classes in tailwind css is matter
+  return twMerge(clsx(inputs))
 }
