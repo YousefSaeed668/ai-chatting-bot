21c8de6621fb6492aff2aab3cc6e05bdeae2adb5
YousefSaeed668 <yousefsaeed668@gmail.com>
2024-02-29 23:15:22 +0200

    fix deploy issue

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,28 +1,41 @@
 "use client";
+import { Loader2 } from "lucide-react";
+
 import { useRouter, useSearchParams } from "next/navigation";
 import { trpc } from "../_trpc/client";
 import { useEffect } from "react";
+
 const Page = () => {
   const router = useRouter();
 
-  const params = useSearchParams();
-  const origin = params.get("origin");
-  const { data, isLoading, status } = trpc.authCallback.useQuery(undefined);
+  const searchParams = useSearchParams();
+  const origin = searchParams.get("origin");
+
+  const { status } = trpc.authCallback.useQuery(undefined, {
+    retry: true,
+    retryDelay: 500,
+  });
 
   useEffect(() => {
     if (status === "success") {
-      // user is synced to db
       router.push(origin ? `/${origin}` : "/dashboard");
     }
-  }, [data, status, origin, router]);
+  }, [status, origin, router]);
 
   useEffect(() => {
     if (status === "error") {
       router.push(origin ? `/${origin}` : "/");
     }
   }, [status, router, origin]);
-
-  return <div></div>;
+  return (
+    <div className="w-full mt-24 flex justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+        <h3 className="font-semibold text-xl">Setting up your account...</h3>
+        <p>You will be redirected automatically.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
